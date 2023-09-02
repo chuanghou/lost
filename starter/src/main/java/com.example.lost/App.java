4e63a1169c4db0f6db0f0234ef874bf076924518
Alexander Kriegisch <Alexander@Kriegisch.name>
2023-09-02 12:45:00 +0700

    Make App do something meaningful
    calling TestController.test() and then end the application.

--- starter/src/main/java/com.example.lost/App.java
+++ starter/src/main/java/com.example.lost/App.java
@@ -1,13 +1,17 @@
 package com.example.lost;
 
+import com.example.lost.controller.TestController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        try (ConfigurableApplicationContext applicationContext = SpringApplication.run(App.class, args)) {
+            System.out.println(applicationContext.getBean(TestController.class).test());
+        }
     }
 
 }
